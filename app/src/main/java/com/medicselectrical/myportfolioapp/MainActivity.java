e4212d862e3c6236b53155baf540b9f02e979ac4
Alexander Sheldon <alex@medicselectrical.com>
2015-09-28 14:47:41 -0600

    Added Pop-up messages when buttons are clicked

--- app/src/main/java/com/medicselectrical/myportfolioapp/MainActivity.java
+++ app/src/main/java/com/medicselectrical/myportfolioapp/MainActivity.java
@@ -1,11 +1,13 @@
 package com.medicselectrical.myportfolioapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,18 +43,48 @@ public class MainActivity extends AppCompatActivity {
     // Button Event Functions
 
     public void onBtnClicked(View v){
-        if(v.getId() == R.id.spotifystreamerbutton){
+        if(v.getId() == R.id.popularmoviesbutton){
+            Context context = getApplicationContext();
+            CharSequence text = "This button will launch my Popular Movies app!";
+            int duration = Toast.LENGTH_SHORT;
 
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         } else if(v.getId() == R.id.scoresappbutton){
+            Context context = getApplicationContext();
+            CharSequence text = "This button will launch my Scores app!";
+            int duration = Toast.LENGTH_SHORT;
 
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         } else if(v.getId() == R.id.libraryapp){
+            Context context = getApplicationContext();
+            CharSequence text = "This button will launch my Library app!";
+            int duration = Toast.LENGTH_SHORT;
 
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         } else if(v.getId() == R.id.builditbigger){
+            Context context = getApplicationContext();
+            CharSequence text = "This button will launch my Build It Bigger app!";
+            int duration = Toast.LENGTH_SHORT;
 
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         } else if(v.getId() == R.id.xyzreader){
+            Context context = getApplicationContext();
+            CharSequence text = "This button will launch my XYZ Reader app!";
+            int duration = Toast.LENGTH_SHORT;
 
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         } else if(v.getId() == R.id.capstonebutton){
+            Context context = getApplicationContext();
+            CharSequence text = "This button will launch my Capstone app!";
+            int duration = Toast.LENGTH_SHORT;
 
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
     }
 }
